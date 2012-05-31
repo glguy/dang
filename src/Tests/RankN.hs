552563aa93c53a18d693fc3360e5f554cbf9207a
Trevor Elliott <trevor@galois.com>
2012-05-31 10:26:23 -0700

    Fix a test case for weakPrenixConversion

--- src/Tests/RankN.hs
+++ src/Tests/RankN.hs
@@ -6,6 +6,7 @@ import Tests.Types
 import TypeChecker.Types
 import TypeChecker.Subsumption
 
+import Data.Maybe (fromMaybe)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.Framework (Test,testGroup)
 import Test.QuickCheck
