95718e762ff7f2e53a2161f459b50bf0f1b78645
Trevor Elliott <awesomelyawesome@gmail.com>
2012-07-06 14:05:26 -0700

    Change renumbering to not rely on ST

--- src/Syntax/Renumber.hs
+++ src/Syntax/Renumber.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Syntax.Renumber where
 
 import QualName (Name)
@@ -6,90 +8,110 @@ import TypeChecker.Types
     (PolyFun(..),Type(..),Index,TParam(..),uvar,TVar(..),Forall(..),Qual(..))
 
 import Control.Monad (ap)
-import Control.Monad.ST (ST,runST)
-import Data.STRef (STRef,newSTRef,readSTRef,writeSTRef)
+import MonadLib (Id,StateT,get,set,runM)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import qualified Data.Traversable as T
 
 
+-- External Interface ----------------------------------------------------------
+
 -- | Perform a syntactic renaming of the type variables.
-renumber :: Renumber a => a -> a
-renumber a = runST body
+renumber :: HasRenumber a => a -> a
+renumber a = fst (runM (unRenumber (renumberVars a)) emptyRW)
+
+
+-- Renumbering Monad -----------------------------------------------------------
+
+newtype Renumber a = Renumber
+  { unRenumber :: StateT RW Id a
+  } deriving (Functor,Monad)
+
+data RW = RW
+  { rwIndex :: !Index
+  , rwNames :: Map.Map Name Index
+  } deriving (Show)
+
+emptyRW :: RW
+emptyRW  = RW
+  { rwIndex = 0
+  , rwNames = Map.empty
+  }
+
+addIndex :: Name -> RW -> (Index,RW)
+addIndex n rw = (rwIndex rw,rw')
   where
-  body = do
-    ref <- initialRenumberState
-    renumberVars ref a
+  rw' = RW
+    { rwIndex = rwIndex rw + 1
+    , rwNames = Map.insert n (rwIndex rw) (rwNames rw)
+    }
+
+lookupIndex :: Name -> Renumber Index
+lookupIndex n = Renumber $ do
+  rw <- get
+  case Map.lookup n (rwNames rw) of
+    Just ix -> return ix
+    Nothing -> do
+      let (ix,rw') = addIndex n rw
+      set rw'
+      return ix
 
 
-type RenumberState s = STRef s (Index, Map.Map Name Index)
+-- Term Renumbering ------------------------------------------------------------
 
-initialRenumberState :: ST s (RenumberState s)
-initialRenumberState  = newSTRef (0, Map.empty)
+class HasRenumber a where
+  renumberVars :: a -> Renumber a
 
+instance (HasRenumber a,HasRenumber b) => HasRenumber (a,b) where
+  renumberVars (a,b) = (,) `fmap` renumberVars a `ap` renumberVars b
 
-class Renumber a where
-  renumberVars :: RenumberState s -> a -> ST s a
+instance HasRenumber a => HasRenumber [a] where
+  renumberVars = T.mapM renumberVars
 
-instance (Renumber a,Renumber b) => Renumber (a,b) where
-  renumberVars st (a,b) =
-    (,) `fmap` renumberVars st a `ap` renumberVars st b
+instance HasRenumber a => HasRenumber (Maybe a) where
+  renumberVars = T.mapM renumberVars
 
-instance Renumber a => Renumber [a] where
-  renumberVars st = T.mapM (renumberVars st)
+instance (Ord a, HasRenumber a) => HasRenumber (Set.Set a) where
+  renumberVars sa = Set.fromList `fmap` mapM renumberVars (Set.toList sa)
 
-instance Renumber a => Renumber (Maybe a) where
-  renumberVars st = T.mapM (renumberVars st)
+instance HasRenumber PolyFun where
+  renumberVars (PolyFun ps ty) =
+    PolyFun `fmap` T.mapM renumberVars ps `ap` renumberVars ty
 
-instance (Ord a, Renumber a) => Renumber (Set.Set a) where
-  renumberVars st sa = do
-    es <- mapM (renumberVars st) (Set.toList sa)
-    return (Set.fromList es)
-
-instance Renumber PolyFun where
-  renumberVars st (PolyFun ps ty) =
-    PolyFun `fmap` T.mapM (renumberVars st) ps `ap` renumberVars st ty
-
-instance Renumber Type where
-  renumberVars ref ty = case ty of
-    TApp l r      -> TApp      `fmap` renumberVars ref l `ap` renumberVars ref r
-    TInfix op l r -> TInfix op `fmap` renumberVars ref l `ap` renumberVars ref r
+instance HasRenumber Type where
+  renumberVars ty = case ty of
+    TApp l r      -> TApp      `fmap` renumberVars l `ap` renumberVars r
+    TInfix op l r -> TInfix op `fmap` renumberVars l `ap` renumberVars r
     TCon _        -> return ty
     TVar (GVar _) -> return ty
-    TVar (UVar p) -> uvar `fmap` renumberVars ref p
+    TVar (UVar p) -> uvar `fmap` renumberVars p
 
-instance Renumber TParam where
-  renumberVars ref p = do
-    (n,m) <- readSTRef ref
-    let var = paramName p
-    case Map.lookup var m of
-      Just ix -> return p { paramIndex = ix }
-      Nothing -> do
-        writeSTRef ref (n+1,Map.insert var n m)
-        return p { paramIndex = n }
+instance HasRenumber TParam where
+  renumberVars p = do
+    ix <- lookupIndex (paramName p)
+    return p { paramIndex = ix }
 
 -- | Make the assumption that the parameters in the forall are already bound
 -- here; renumbering should only happen on types that haven't already had a
 -- forall put on them.
-instance Renumber a => Renumber (Forall a) where
-  renumberVars st f = upd `fmap` renumberVars st (forallData f)
+instance HasRenumber a => HasRenumber (Forall a) where
+  renumberVars f = upd `fmap` renumberVars (forallData f)
     where
     upd a = f { forallData = a }
 
-instance Renumber a => Renumber (Qual a) where
-  renumberVars st (Qual cxt a) =
-    Qual `fmap` renumberVars st cxt `ap` renumberVars st a
+instance HasRenumber a => HasRenumber (Qual a) where
+  renumberVars (Qual cxt a) = Qual `fmap` renumberVars cxt `ap` renumberVars a
 
-instance Renumber ConstrGroup where
-  renumberVars st cg = do
-    tys' <- renumberVars st (groupArgs cg)
-    cs'  <- T.mapM (renumberVars st) (groupConstrs cg)
+instance HasRenumber ConstrGroup where
+  renumberVars cg = do
+    tys' <- renumberVars (groupArgs cg)
+    cs'  <- T.mapM renumberVars (groupConstrs cg)
     return cg
       { groupArgs    = tys'
       , groupConstrs = cs'
       }
 
-instance Renumber Constr where
-  renumberVars st c = do
-    fs' <- T.mapM (renumberVars st) (constrFields c)
+instance HasRenumber Constr where
+  renumberVars c = do
+    fs' <- T.mapM renumberVars (constrFields c)
     return c { constrFields = fs' }
